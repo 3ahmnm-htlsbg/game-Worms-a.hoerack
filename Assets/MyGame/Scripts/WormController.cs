462ba893e93a272c5c9d4474c3a2f58da4a45fe4
ahoerack03 <anna.hoerack2003@gmail.com>
2020-01-21 15:05:38 +0100

    Add WormController Script

--- Assets/MyGame/Scripts/WormController.cs
+++ Assets/MyGame/Scripts/WormController.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class WormController : MonoBehaviour
 {
+    public int x;
+    public string text;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        if(x>0) {
+            Debug.Log("x ist größer als 0");
+    }    
+        else {
+            Debug.Log("x ist kleiner als 0");
     }
+        if(text == "nani") {
+            Debug.Log("In der text Variable steht nani");
+    }    
+        else {
+            Debug.Log("In der text Variable steht etwas anderes");
+  }    
+    }
+
 
     // Update is called once per frame
     void Update()
